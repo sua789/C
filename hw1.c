a94195e3c90b877401df368d7062370df46b795d
sua789 <sua-jung0322@naver.com>
2023-09-20 22:33:50 +0900

    Add Text File [hw1.c]

--- hw1.c
+++ hw1.c
@@ -1,8 +1,15 @@
-﻿빌드 시작...
-1>------ 빌드 시작: 프로젝트: Project1, 구성: Debug x64 ------
-1>hw1.c
-1>C:\Users\SUA\source\repos\Project1\Project1\hw1.c(7,2): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
-1>Project1.vcxproj -> C:\Users\SUA\source\repos\Project1\x64\Debug\Project1.exe
-1>"Project1.vcxproj" 프로젝트를 빌드했습니다.
-========== 빌드: 1개 성공, 0개 실패, 0개 최신 상태, 0개 건너뜀 ==========
-========== 빌드이(가) 8:28 PM에 시작되었고 01.135 초이(가) 소요됨 ==========
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2;
+	printf("Input two integers: ");
+	scanf("%d %d", &num1, &num2);
+	int result1 = num1 & num2;
+	int result2 = num1 | num2;
+	int result3 = num1 ^ num2;
+	printf("%d & %d = %d \n", num1, num2, result1);
+	printf("%d | %d = %d \n", num1, num2, result2);
+	printf("%d ^ %d = %d \n", num1, num2, result3);
+	return 0;
+}
